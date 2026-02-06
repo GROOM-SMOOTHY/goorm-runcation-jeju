ea261f1a37af64c5ce1a6cf044c1e6421f4f4d54
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-02-07 04:11:12 +0900

    feat : app.tsx Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
+// import LoginPage from "@/pages/LoginPage";
 import NotFoundPage from "@/pages/NotFoundPage";
 import { useEffect } from "react";
-import { supabase } from "./lib/supabase";
+import { supabase } from "@/lib/supabase";
+import BackgroundLayout from "@/components/layout/BackgroundLayout";
 
 function App() {
   // Supabase 연결 확인 (앱 로드 시 한 번만)
@@ -18,10 +20,18 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="*" element={<NotFoundPage />} />
+      <Route element={<BackgroundLayout />}>
+        {/* 첫 페이지 = 로그인 */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* 로그인 성공 후 메인 페이지 */}
+        <Route path="/home" element={<HomePage />} />
+
+        {/* 404 */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
     </Routes>
   );
-
 }
+
 export default App;
