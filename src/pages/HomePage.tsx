d439ffec38cf484feabe7a9e675615e211e64918
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-02-07 06:27:21 +0900

    feat : HomePage TapNav 적용

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,16 @@
+import TopNav from "@/components/layout/TopNavigation";
+import orange from "@/assets/orange.png";
+
 export default function HomePage() {
-    return <>
-    
+  return (
+    <>
+      <TopNav
+        onBack={() => console.log("뒤로가기")}
+        rightElement={<img src={orange} width={50} height={50} />}
+      />
+      <main style={{ padding: "16px" }}>
+        <h1>홈</h1>
+      </main>
     </>
+  );
 }
