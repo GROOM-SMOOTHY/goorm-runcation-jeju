590c9f068344f2517d8f83bdb8939f5763a5fbd9
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-02-07 07:12:53 +0900

    refact : TopNav 구조개선

--- src/components/layout/TopNavigation.tsx
+++ src/components/layout/TopNavigation.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import styles from "@/components/layout/TopNavigation.module.css";
+import { useNavigate } from "react-router-dom";
 
 interface TopNavigationProps {
+  // 타입정의
   title?: string;
   onBack?: () => void;
   rightElement?: React.ReactNode; // 오른쪽 아이콘/버튼
   style?: React.CSSProperties;
+  
 }
 
 const TopNavigation: React.FC<TopNavigationProps> = ({
@@ -13,41 +17,20 @@ const TopNavigation: React.FC<TopNavigationProps> = ({
   rightElement,
   style,
 }) => {
-  const containerStyle: React.CSSProperties = {
-    width: "100%",
-    height: "50px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    background: "transparent", // 투명 배경
-    padding: "0 12px",
-  };
-
-  const backButtonStyle: React.CSSProperties = {
-    fontSize: "25px",
-    color: "#656565",
-    cursor: "pointer",
-    userSelect: "none",
-  };
-
-  const titleStyle: React.CSSProperties = {
-    fontSize: "22px",
-    color: "#656565",
-    fontWeight: 400,
-  };
+  const navigate = useNavigate();
 
   return (
-    <div style={{ ...containerStyle, ...style }}>
-      {/* Left Back Button */}
-      <div onClick={onBack} style={backButtonStyle}>
+    <div className={styles.topNavigation} style={style}>
+      {/* 뒤로가기 */}
+      <div onClick={onBack? onBack : () => navigate(-1)} className={styles.back}>
         {onBack ? "<" : ""}
       </div>
 
-      {/* Title */}
-      <div style={titleStyle}>{title}</div>
+      {/* 제목 */}
+      <div className={styles.title}>{title}</div>
 
-      {/* Right Icon */}
-      <div>{rightElement}</div>
+      {/* 오렌지 아이콘 */}
+      <div className={styles.right}>{rightElement}</div>
     </div>
   );
 };
