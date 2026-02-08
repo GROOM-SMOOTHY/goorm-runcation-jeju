0994d8d2a2c89b50c63ca9630dfbe12c3bf58749
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-02-09 00:11:39 +0900

    feat : 백그라운드 소셜 아이콘 링크 연결

--- src/components/layout/BackgroundLayout.tsx
+++ src/components/layout/BackgroundLayout.tsx
@@ -3,8 +3,11 @@ import styles from "@/components/layout/BackgroundLayout.module.css";
 import { FaDownload, FaGithub } from "react-icons/fa";
 import { SiNotion } from "react-icons/si";
 
-// 소셜 아이콘 배열
-const socialIcons = [FaGithub, SiNotion];
+// 소셜 아이콘 + 링크 배열
+const socialLinks = [
+  { Icon: FaGithub, url: "https://github.com/GROOM-SMOOTHY/goorm-runcation-jeju" },
+  { Icon: SiNotion, url: "https://www.notion.so/Team-Project-SMOOTHY-2bf878f91ae480fe948edbb2967a4bcc?source=copy_link" },
+];
 
 const BackgroundLayout = () => {
   return (
@@ -26,19 +29,27 @@ const BackgroundLayout = () => {
 
         {/* 소셜 아이콘 */}
         <div className={styles.socialIcons}>
-          {socialIcons.map((Icon, index) => (
-            <Icon key={index} className={styles.socialIcon} />
+          {socialLinks.map(({ Icon, url }, index) => (
+            <a
+              key={index}
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.socialLink}
+              aria-label="소셜 링크"
+            >
+              <Icon className={styles.socialIcon} />
+            </a>
           ))}
         </div>
 
         {/* 소개 텍스트 */}
         <p className={styles.footerText}>
-          우리는 구름 DEEP DIVE에서 함께 협업하며 하나의 서비스를 완성하는 학습 기반 프로젝트 팀입니다.<br/>  
-          각자 다른 배경과 강점을 가진 팀원들이 모여, 사용자에게 실질적이고 가치 있는 경험을 제공하는 것을 목표로 개발했습니다.
-          이번 프로젝트를 통해 우리는 기술 역량뿐 아니라 소통, 문제 해결, 역할 분담, 협업에 대한<br/>
-          깊은 이해를 쌓고 있습니다.   
-          기수와 팀이 달라도, 모두가 같은 목표 아래 더 나은 서비스를 만들어가기 위해 함께 성장하고 있습니다.<br/>  
-
+          우리는 구름 DEEP DIVE에서 함께 협업하며 하나의 서비스를 완성하는 학습 기반<br /> 프로젝트 팀입니다.
+          각자 다른 배경과 강점을 가진 팀원들이 모여, 사용자에게 실질적이고<br /> 가치 있는 경험을 제공하는 것을 목표로 개발했습니다.
+          이번 프로젝트를 통해 우리는<br /> 기술 역량뿐 아니라 소통, 문제 해결, 역할 분담, 협업에 대한
+          깊은 이해를 쌓고 있습니다.<br />   
+          기수와 팀이 달라도, 모두가 같은 목표 아래 더 나은 서비스를 만들어가기 위해 함께 성장하고 있습니다.<br />
         </p>
       </div>
 
