b2490786e6a23deabccdb845ca25c9adcad6ec09
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-02-12 01:06:00 +0900

    [ Feat ] Header 컴포넌트 구현 및 레이아웃 정리 #Tsk 12 (#33)
    * 스토리북 불필요한 코드 제거 (#30)
    
    * add: story book 라이브러리 추가
    
    * feat: story book 기본 설정
    
    * style: 컬러 토큰 값 추가
    
    * storybook: 컬러 토큰 mdx 파일 추가
    
    * feat: story book 설정 추가
    
    * refactor: storybook 불필요한 코드 제거
    
    * fest : Radix-ui 설치 (#31)
    
    * refactor : 프로젝트 폴더 구조 개선
    
    * feat : radix-ui/react-icons 설치 밋 header 구현
    
    * refactor : 프로젝트 폴더 구조 개선
    
    * refactor : 프로젝트 폴더 구조 개선
    
    * refactor : 프로젝트 폴더 구조 개선
    
    * refactor : header 경로 재설정
    
    * refactor :  리뷰 반영
    
    ---------
    
    Co-authored-by: Lee_yam <48716775+Leemainsw@users.noreply.github.com>

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { ChevronLeftIcon } from "@radix-ui/react-icons";
+import styles from "@/components/header.module.css";
+
+interface HeaderProps {
+  title?: string;
+  onBack?: () => void;
+  backTextColor?: string;
+  style?: React.CSSProperties;
+}
+
+const Header: React.FC<HeaderProps> = ({
+  title = "",
+  onBack,
+  backTextColor = "var(--bg-black)",
+  style,
+}) => {
+
+  return (
+    <header className={styles.topNavigation} style={style}>
+      { onBack && (
+        <button
+          type="button"
+          onClick={onBack ?? (() => window.history.back())}
+          className={styles.back}
+          aria-label="뒤로가기"
+          style={{ color: backTextColor }}
+        >
+          <ChevronLeftIcon width={28} height={28} />
+        </button>
+      )}
+      
+      <div className={styles.title}>{title}</div>
+    </header>
+  );
+};
+
+export default Header;
