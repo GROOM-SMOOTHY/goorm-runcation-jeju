cf40cccbc5e0ffcdc83ea386cb78c63f47f357c5
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-02-12 23:27:51 +0900

    refactor : 경로 시작

--- src/stories/components/Modal.stories.tsx
+++ src/stories/components/Modal.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { useState } from "react";
-import Modal from "@/components/common/Modal";
-import Button from "@/components/common/Button";
+import Modal from "@/components/common/Modal/Modal.css";
+import Button from "@/components/common/Button/Button";
 
 const meta = {
   title: "Components/Modal",
