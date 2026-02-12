8915f79aa8e63954abe1a7add3400869c86915aa
GwonWooL <229861@naver.com>
2026-02-12 17:56:59 +0900

    feat: components-BottomNaviagtion 구현

---
+++ src/components/BottomNavigation/BottomNavigation.tsx
@@ -0,0 +1,32 @@
+import "./buttonNavigation.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiMapPin } from "react-icons/bi";
+import { MdOutlinePayments } from "react-icons/md";
+import { BiWallet } from "react-icons/bi";
+import { BsPerson } from "react-icons/bs";
+
+export default function ButtonNavigation() {
+  return (
+    <nav className="button-navigation">
+      <div className="nav-item home">
+        <AiOutlineHome size={20} />
+        <span>홈</span>
+      </div>
+      <div className="nav-item pin">
+        <BiMapPin size={20} />
+        <span>도장깨기</span>
+      </div>
+      <div className="nav-item payment">
+        <MdOutlinePayments size={32} />
+      </div>
+      <div className="nav-item wallet">
+        <BiWallet size={20} />
+        <span>정산현황</span>
+      </div>
+      <div className="nav-item person">
+        <BsPerson size={20} />
+        <span>마이</span>
+      </div>
+    </nav>
+  );
+}
