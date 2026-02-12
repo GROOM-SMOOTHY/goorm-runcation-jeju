323a90a968a299890932cecf603ed2026b39c9d1
GwonWooL <229861@naver.com>
2026-02-12 14:28:55 +0900

    fix: Button 컴포넌트에 type 기본값 추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,15 +5,17 @@ interface ButtonProps {
   children: ReactNode;
   variant?: "primary" | "default";
   onClick?: () => void;
+  type: "button";
 }
 
 export default function Button({
   children,
   variant = "default",
   onClick,
+  type = "button",
 }: ButtonProps) {
   return (
-    <button className={`btn ${variant}`} onClick={onClick}>
+    <button type={type} className={`btn ${variant}`} onClick={onClick}>
       {children}
     </button>
   );
