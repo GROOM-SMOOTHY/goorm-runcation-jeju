b4ee2cdabba7145f2a6baac515632f52a72f2a24
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-02-12 09:42:45 +0900

    refactor: CodeInput 컴포넌트 구조 개선 및 불필요 로직 제거

--- src/components/common/CodeInput.tsx
+++ src/components/common/CodeInput.tsx
@@ -1,36 +1,27 @@
-import * as React from "react";
 import { unstable_OneTimePasswordField as OneTimePasswordField } from "radix-ui";
 import style from "@/components/common/CodeInput.module.css";
 
 export default function CodeInput() {
-  // const OTP_LENGTH = 6;
-  // const inputRefs = React.useRef<(HTMLInputElement | null)[]>([]);
-
-  // // const handleChange = (index: number, value: string) => {
-  // //   if (value.length > 0 && index < OTP_LENGTH - 1) {
-  // //     inputRefs.current[index + 1]?.focus();
-  // //   }
-  // // };
-
-  // // const handleKeyDown = (index: number, e: React.KeyboardEvent<HTMLInputElement>) => {
-  // //   if (e.key === "Backspace" && !e.currentTarget.value && index > 0) {
-  // //     inputRefs.current[index - 1]?.focus();
-  // //   }
-  // // };
+  const OTP_LENGTH = 6;
 
   return (
     <div className={style.OTPcontener}>
       <div className={style.labelContener}>
-        <label className={style.label}>인증 코드</label>
+        <label className={style.label}>인증코드</label>
       </div>
-      
-      <form onSubmit={(e) => e.preventDefault()} >
-        <OneTimePasswordField.Root name="otp" className={style.OTPGroup}>
-          {[...Array(6)].map((_, i) => (
-            <OneTimePasswordField.Input 
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <OneTimePasswordField.Root
+          name="otp"
+          className={style.OTPGroup}
+          autoComplete="one-time-code"
+        >
+          {[...Array(OTP_LENGTH)].map((_, i) => (
+            <OneTimePasswordField.Input
               key={i}
               className={style.OTPInput}
-              maxLength={1}
+              inputMode="numeric"
+              pattern="[0-9]*"
             />
           ))}
           <OneTimePasswordField.HiddenInput />
