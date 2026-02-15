bd6f67c2769581ce46f10c0e6309d7aaa44b5f5e
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-02-16 06:54:11 +0900

    [ Feat ] StoreInfoCard 컴포넌트 구현 #TSK-63 (#62)
    * feat : 타입스크립트 .module.css 이거 못찾아서 추가함
    
    * feat : StoreMap 및 카카오맵 api 구현
    
    * refactor : 길찾기 버튼 컴포넌트로 추후 추가할꺼라 뺐음
    
    * feat: React Kakao Maps 컴포넌트 개선 및 마커 재사용
    
    * refactor : 코드래빗 리뷰 반영
    
    * refactor : 코드래빗 리뷰 반영
    
    * feat : StoreInfoCard 구현
    
    * refactor : 주석 정리
    
    * refactor : css 업데이트
    
    * refactor : 코드래빗 리뷰 반영
    
    * refactor : 코드래빗 리뷰 반영
    
    * refactor : 코드래빗 리뷰 반영

---
+++ src/stories/components/pages/restaurant-store-page/StoreMapCard/StoreMap.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import StoreMap from "@/components/pages/restaurant-store-page/StoreMap/StoreMap";
+
+const meta = {
+  title: "Pages/Restaurant-store-page/StoreMap",
+  component: StoreMap,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    latitude: { control: "number" },
+    longitude: { control: "number" },
+    storeName: { control: "text" },
+  },
+} satisfies Meta<typeof StoreMap>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    latitude: 37.5665,
+    longitude: 126.9780,
+    storeName: "맛있는 식당",
+  },
+};
