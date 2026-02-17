7fccf3b26036223e420803c3f856a648e1fb5a54
Leemainsw <yebom6476@gmail.com>
2026-02-18 04:02:47 +0900

    feat: 유저명 수정

--- src/components/pages/settlement-add-history-page/PaymentsMembers/data.ts
+++ src/components/pages/settlement-add-history-page/PaymentsMembers/data.ts
@@ -2,27 +2,27 @@ export const mockMembers: Member[] = [
     {
         userId: 1,
         profileSrc: "https://cdn.sisaweek.com/news/photo/202308/207227_209449_3341.jpg",
-        name: "John Doe"
+        name: "김철수"
     },
     {
         userId: 2,
         profileSrc: "https://cdn.sisaweek.com/news/photo/202308/207227_209449_3341.jpg",
-        name: "Ye Kim"
+        name: "이경호"
     },
     {
         userId: 3,
         profileSrc: "https://cdn.sisaweek.com/news/photo/202308/207227_209449_3341.jpg",
-        name: "Jim Beam"
+        name: "박영희"
     },
     {
         userId: 4,
         profileSrc: "https://cdn.sisaweek.com/news/photo/202308/207227_209449_3341.jpg",
-        name: "Rim So"
+        name: "최민수"
     },
     {
         userId: 5,
         profileSrc: "https://cdn.sisaweek.com/news/photo/202308/207227_209449_3341.jpg",
-        name: "Min Yeong Kim"
+        name: "김나영"
     },
 ]
 
