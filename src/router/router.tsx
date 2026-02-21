52b504e52ff94f955c1d0f9d5af4a164ddc90c79
GwonWooL <229861@naver.com>
2026-02-22 00:47:28 +0900

    시작 페이지('/start') UI 구현

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -7,15 +7,16 @@ import TravelPage from "@/pages/TravelPage";
 import StampPage from "@/pages/StampPage";
 import GroupPage from "@/pages/GruopPage/GroupPage";
 import MainPage from "@/pages/MainPage/MainPage";
-import RestaurantStorePage from "@/pages/RestaurantStorePage/RestaurantStorePage"
-import RestaurantListPage from "@/pages/RestaurantListPage/RestaurantListPage"
+import RestaurantStorePage from "@/pages/RestaurantStorePage/RestaurantStorePage";
+import RestaurantListPage from "@/pages/RestaurantListPage/RestaurantListPage";
+import StartPage from "@/pages/StartPage/StartPage";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route element={<BackgroundLayout />}>
         {/* 첫 페이지 = 로그인 */}
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<StartPage />} />
 
         {/* 로그인 성공 후 메인 페이지 */}
         <Route path="/home" element={<HomePage />} />
@@ -27,20 +28,23 @@ export default function AppRouter() {
         <Route path="/travel" element={<TravelPage />} />
 
         {/* 그룹 페이지 */}
-        <Route path='/group' element={<GroupPage />} />
+        <Route path="/group" element={<GroupPage />} />
 
         {/* 메인 페이지 */}
-        <Route path='/main' element={<MainPage />} />
+        <Route path="/main" element={<MainPage />} />
 
         {/* 맛집 리스트 페이지 */}
         <Route path="/restaurants" element={<RestaurantListPage />} />
-        
+
         {/* 맛집 리스트 상세 페이지 */}
-        <Route path="/restaurants/:id-:slug" element={<RestaurantStorePage />} />
-        
+        <Route
+          path="/restaurants/:id-:slug"
+          element={<RestaurantStorePage />}
+        />
+
         {/* 404 */}
         <Route path="*" element={<NotFoundPage />} />
-      </Route >
-    </Routes >
+      </Route>
+    </Routes>
   );
 }
