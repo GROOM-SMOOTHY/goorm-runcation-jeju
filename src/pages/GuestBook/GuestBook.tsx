e8a809eb2388c85ea5127c9604732137438cd446
GwonWooL <229861@naver.com>
2026-02-23 00:54:48 +0900

    feat: 방명록 작성하기 페이지 (’/guestbook’) UI 구현

---
+++ src/pages/GuestBook/GuestBook.tsx
@@ -0,0 +1,71 @@
+import styles from "@/pages/GuestBook/GuestBook.module.css";
+
+import Header from "@/components/layout/Header/Header";
+import BottomNavigation from "@/components/common/BottomNavigation/BottomNavigation";
+import Button from "@/components/common/Button/Button";
+import AddPicture from "@/components/pages/guestbook/AddPicture/AddPicture";
+
+import { useState } from "react";
+
+export default function GuestBook() {
+  const [images, setImages] = useState<string[]>([]);
+  const [content, setContent] = useState("");
+
+  const onAdd = (url: string) => {
+    if (images.length >= 4) return;
+    setImages((prev) => [...prev, url]);
+  };
+  const onRemove = (index: number) => {
+    setImages((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  const onClick = () => {
+    if (images.length === 0) {
+      alert("사진을 최소 1장 이상 첨부해주세요");
+      return;
+    }
+
+    if (content.trim() === "") {
+      alert("내용을 입력해주세요");
+      return;
+    }
+
+    alert("방명록이 등록되었습니다");
+  };
+
+  return (
+    <div className={styles.container}>
+      <Header title="방명록" />
+      <div className={styles.box}>
+        <div className={styles.location}>
+          <div className={styles.img}>
+            <img src="/src/assets/Overlay.png" />
+          </div>
+          <div className={styles.title}>
+            <p>애월읍, 제주도</p>
+            <p>TODAY IN JEJU</p>
+          </div>
+        </div>
+        <div className={styles.desc}>
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="제주 런케이션은 어땠나요?"
+            className={styles.textarea}
+          />
+        </div>
+        <div className={styles.picture}>
+          <AddPicture images={images} onAdd={onAdd} onRemove={onRemove} />
+        </div>
+        <div className={styles.button}>
+          <Button type="button" variant="primary" onClick={onClick}>
+            방명록 등록하기
+          </Button>
+        </div>
+      </div>
+      <div className={styles.botNav}>
+        <BottomNavigation />
+      </div>
+    </div>
+  );
+}
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -7,8 +7,9 @@ import TravelPage from "@/pages/TravelPage";
 import StampPage from "@/pages/StampPage";
 import GroupPage from "@/pages/GruopPage/GroupPage";
 import MainPage from "@/pages/MainPage/MainPage";
-import RestaurantStorePage from "@/pages/RestaurantStorePage/RestaurantStorePage"
-import RestaurantListPage from "@/pages/RestaurantListPage/RestaurantListPage"
+import RestaurantStorePage from "@/pages/RestaurantStorePage/RestaurantStorePage";
+import RestaurantListPage from "@/pages/RestaurantListPage/RestaurantListPage";
+import GuestBook from "@/pages/GuestBook/GuestBook";
 
 export default function AppRouter() {
   return (
@@ -16,31 +17,28 @@ export default function AppRouter() {
       <Route element={<BackgroundLayout />}>
         {/* 첫 페이지 = 로그인 */}
         <Route path="/" element={<LoginPage />} />
-
         {/* 로그인 성공 후 메인 페이지 */}
         <Route path="/home" element={<HomePage />} />
-
         {/* 지역 스탬프 */}
         <Route path="/stamp" element={<StampPage />} />
-
         {/* 메인 */}
         <Route path="/travel" element={<TravelPage />} />
-
         {/* 그룹 페이지 */}
-        <Route path='/group' element={<GroupPage />} />
-
+        <Route path="/group" element={<GroupPage />} />
         {/* 메인 페이지 */}
-        <Route path='/main' element={<MainPage />} />
-
+        <Route path="/main" element={<MainPage />} />
+        {/* 방명록 페이지 */}ㅗ
+        <Route path="/guestbook" element={<GuestBook />} />
         {/* 맛집 리스트 페이지 */}
         <Route path="/restaurants" element={<RestaurantListPage />} />
-        
         {/* 맛집 리스트 상세 페이지 */}
-        <Route path="/restaurants/:id-:slug" element={<RestaurantStorePage />} />
-        
+        <Route
+          path="/restaurants/:id-:slug"
+          element={<RestaurantStorePage />}
+        />
         {/* 404 */}
         <Route path="*" element={<NotFoundPage />} />
-      </Route >
-    </Routes >
+      </Route>
+    </Routes>
   );
 }
