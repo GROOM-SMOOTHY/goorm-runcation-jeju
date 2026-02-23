87e0e488287be1038af7a2495cda7063730ce5c4
Leemainsw <yebom6476@gmail.com>
2026-02-23 23:55:59 +0900

    feat: 마이페이지 스타일 수정 및 프로필 기본 사진 설정

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,19 +1,19 @@
-import styles from "@/pages/MyPage/MyPage.module.css";
-import Header from "@/components/layout/Header/Header";
-import MyProfilePicture from "@/components/pages/Mypage/MyProfilePicture/MyProfilePicture";
-import TipBox from "@/components/pages/Mypage/TipBox/TipBox";
-import Button from "@/components/common/Button/Button";
-import ButtonNavigation from "@/components/common/BottomNavigation/BottomNavigation";
-import MyPageInput from "./MyPageInput";
+import styles from '@/pages/MyPage/MyPage.module.css';
+import Header from '@/components/layout/Header/Header';
+import MyProfilePicture from '@/components/pages/Mypage/MyProfilePicture/MyProfilePicture';
+import TipBox from '@/components/pages/Mypage/TipBox/TipBox';
+import Button from '@/components/common/Button/Button';
+import ButtonNavigation from '@/components/common/BottomNavigation/BottomNavigation';
 
-import { useState } from "react";
+import { useState } from 'react';
+import Input from '@/components/common/Input/Input';
 
 export default function MyPage() {
-  const [name, setName] = useState("");
-  const [tel, setTel] = useState("");
-  const [bank, setBank] = useState("");
-  const [account, setAccount] = useState("");
-  const [depositor, setDepositor] = useState("");
+  const [name, setName] = useState('');
+  const [tel, setTel] = useState('');
+  const [bank, setBank] = useState('');
+  const [account, setAccount] = useState('');
+  const [depositor, setDepositor] = useState('');
 
   const isVaild =
     name.trim() &&
@@ -24,10 +24,10 @@ export default function MyPage() {
 
   const onClick = () => {
     if (!isVaild) {
-      alert("모든 정보를 입력해주세요!");
+      alert('모든 정보를 입력해주세요!');
       return;
     }
-    alert("저장되었습니다!");
+    alert('저장되었습니다!');
   };
   return (
     <div className={styles.container}>
@@ -40,12 +40,43 @@ export default function MyPage() {
         </div>
 
         <div className={styles.input}>
-          <MyPageInput type="name" value={name} onChange={setName} />
-          <MyPageInput type="tel" value={tel} onChange={setTel} />
-          <MyPageInput type="bank" value={bank} onChange={setBank} />
-          <MyPageInput type="account" value={account} onChange={setAccount} />
-          <MyPageInput
-            type="depositor"
+          <Input
+            name="name"
+            label="이름"
+            type="text"
+            placeholder="닉네임"
+            value={name}
+            onChange={setName}
+          />
+          <Input
+            name="tel"
+            label="연락처"
+            type="tel"
+            placeholder="연락처"
+            value={tel}
+            onChange={setTel}
+          />
+          <Input
+            name="bank"
+            label="은행"
+            type="text"
+            placeholder="은행"
+            value={bank}
+            onChange={setBank}
+          />
+          <Input
+            name="account"
+            label="계좌번호"
+            type="text"
+            placeholder="계좌번호"
+            value={account}
+            onChange={setAccount}
+          />
+          <Input
+            name="depositor"
+            label="예금주"
+            type="text"
+            placeholder="예금주"
             value={depositor}
             onChange={setDepositor}
           />
