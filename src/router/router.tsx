e74cf1860b2329c867284ffc171d552a33d00a72
Leemainsw <yebom6476@gmail.com>
2026-02-28 02:47:42 +0900

    refactor: 스타일 수정

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -7,7 +7,6 @@ import StampPage from "@/pages/StampPage";
 import GroupPage from "@/pages/GroupPage/GroupPage";
 import MainPage from "@/pages/MainPage/MainPage";
 import SignUp from "@/pages/SignUp/SignUp";
-import GroupJoinPage from "@/pages/GroupJoinPage";
 
 import LoginPage from "@/pages/LoginPage/LoginPage";
 import SettlementMainPage from "@/pages/SettlementPage/SettlementMainPage/SettlementMainPage";
@@ -21,6 +20,8 @@ import StartPage from "@/pages/StartPage/StartPage";
 import MyPage from "@/pages/MyPage/MyPage";
 import GuestBook from "@/pages/GuestBook/GuestBook";
 import GroupJoinPage from "@/pages/GroupJoinPage";
+import AddStampPage from "@/pages/AddStampPage";
+import ServerError from "@/pages/ServerError/ServerError";
 
 export default function AppRouter() {
   return (
