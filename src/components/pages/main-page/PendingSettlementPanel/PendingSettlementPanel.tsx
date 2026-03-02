7b0e7b00999fb14c5db83b3b656d7647545378d1
Leemainsw <yebom6476@gmail.com>
2026-03-02 21:41:33 +0900

    feat: 클릭 영역 Link로 수정

--- src/components/pages/main-page/PendingSettlementPanel/PendingSettlementPanel.tsx
+++ src/components/pages/main-page/PendingSettlementPanel/PendingSettlementPanel.tsx
@@ -1,15 +1,13 @@
+import { Link } from "react-router-dom";
 import { FaRegBell } from "react-icons/fa";
 import styles from "./PendingSettlementPanel.module.css";
-import { useNavigate } from "react-router-dom";
 
 interface Props {
   count: number;
 }
 export default function PendingSettlementPanel({ count }: Props) {
-  const navigate = useNavigate();
-
   return (
-    <div className={styles.container} onClick={() => navigate("/settlement")}>
+    <Link to="/settlement" className={styles.container}>
       <div className={styles.icon}>
         <FaRegBell size={20} style={{ color: "var(--badge-label-orange)" }} />
       </div>
@@ -17,6 +15,6 @@ export default function PendingSettlementPanel({ count }: Props) {
         <span className={styles.title}>SETTLEMENT</span>
         <span className={styles.description}>정산 대기 {count}건</span>
       </div>
-    </div>
+    </Link>
   );
 }
