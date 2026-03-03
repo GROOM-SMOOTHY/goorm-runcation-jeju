11d2a1f5b59df8fc9e7d7a48515e36c7a2112605
Leemainsw <yebom6476@gmail.com>
2026-03-04 04:14:35 +0900

    feat: filter toggle 컴포넌트 추가

---
+++ src/components/common/FilterToggle/FilterToggle.tsx
@@ -0,0 +1,25 @@
+import type { ComponentProps } from "react";
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
+import styles from "./FilterToggle.module.css";
+
+export const FilterToggleRoot = ({
+  className,
+  type = "single",
+  ...props
+}: ComponentProps<typeof ToggleGroup.Root>) => (
+  <ToggleGroup.Root
+    type={type}
+    className={className ? `${styles.list} ${className}` : styles.list}
+    {...props}
+  />
+);
+
+export const FilterToggleItem = ({
+  className,
+  ...props
+}: ComponentProps<typeof ToggleGroup.Item>) => (
+  <ToggleGroup.Item
+    className={className ? `${styles.trigger} ${className}` : styles.trigger}
+    {...props}
+  />
+);
