745d54557d0847e1fa27e9d9fc3ca614003dcc06
Leemainsw <yebom6476@gmail.com>
2026-03-04 06:18:06 +0900

    feat: 내 정산 상태 업데이트 함수 추가

--- src/components/pages/settlement-history-page/SettleCard/useSettleCard.tsx
+++ src/components/pages/settlement-history-page/SettleCard/useSettleCard.tsx
@@ -1,7 +1,10 @@
 import {
   getExpenseDetail,
+  updateMySettleStatus,
   type ExpenseDetail,
+  type ExpenseParticipantRow,
 } from "@/services/expensesService";
+import { useUser } from "@/store";
 import { useEffect, useState } from "react";
 
 interface Props {
@@ -11,19 +14,43 @@ interface Props {
 
 export default function useSettleCard({ expanded, expenseId }: Props) {
   const [expense, setExpense] = useState<ExpenseDetail | null>(null);
-
+  const [members, setMembers] = useState<ExpenseParticipantRow[]>([]);
+  const { id: userId } = useUser((state) => state);
   useEffect(() => {
     if (!expenseId || !expanded) return;
     const fetchExpenseDetail = async () => {
       const expenseDetail = await getExpenseDetail(expenseId);
       setExpense(expenseDetail);
+      setMembers(expenseDetail.participants);
     };
 
     fetchExpenseDetail();
   }, [expenseId, expanded]);
 
+  const handleMySettleStatusToggle = async (state: "COMPLETE" | "PENDING") => {
+    if (!expense) return;
+
+    const result = await updateMySettleStatus(expense.id, userId, state);
+
+    if (result) {
+      const myInfoIndex = members.findIndex((m) => m.user_id === userId);
+
+      if (myInfoIndex !== -1) {
+        setMembers((prev) => {
+          const newMembers = [...prev];
+          newMembers[myInfoIndex] = {
+            ...newMembers[myInfoIndex],
+            state,
+          };
+          return newMembers;
+        });
+      }
+    }
+  };
+
   return {
     expense,
-    members: expense?.participants ?? [],
+    members,
+    handleMySettleStatusToggle,
   };
 }
