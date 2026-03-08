422eff9a25e5615994f8d7307f736d30d49ae32f
GwonWooL <229861@naver.com>
2026-03-08 20:47:32 +0900

    fix: CodeInput grid column을 length 기반으로 동적 처리

--- src/components/common/CodeInput/CodeInput.tsx
+++ src/components/common/CodeInput/CodeInput.tsx
@@ -75,7 +75,10 @@ export default function CodeInput({
         <label className={style.label}>인증코드</label>
       </div>
 
-      <div className={style.OTPGroup}>
+      <div
+        className={style.OTPGroup}
+        style={{ gridTemplateColumns: `repeat(${length}, 1fr)` }}
+      >
         {[...Array(length)].map((_, i) => (
           <input
             key={i}
